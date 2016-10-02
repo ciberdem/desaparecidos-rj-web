78fe38228fcd9cfe702ab1f6151e19f2222df92d
Vinicius Rodrigues <vini.rodrigues.90@gmail.com>
2016-10-01 22:54:16 -0300

    Add more tests to Person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -4,10 +4,28 @@ require 'test_helper'
 class PersonTest < ActiveSupport::TestCase
   def setup
     @person = Person.new
+    
+    @person_with_attributes = Person.new(
+             name: 'John Doe',
+              age: 19,
+           height: 1.70,
+             skin: 'Branca',
+             eyes: 'Cinzentos',
+              sex: 'Masculino',
+           father: 'Jax Doe',
+           mother: 'Jane Doe',
+             hair: 'Preto',
+      specificity: 'Cicatriz',
+            photo: '/uploads/person/photo/1/photo.jpg'
+    )
   end
 
-  test 'valid person' do
+  test 'valid empty person' do
     assert @person.valid?
+  end
+
+  test 'valid person with attributes' do
+    assert @person_with_attributes.valid?
   end
 
   test '#photo_url' do
