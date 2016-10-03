da07b2375001a25c1e4b4ce6efbfb2ed541827aa
Vinicius Rodrigues <vini.rodrigues.90@gmail.com>
2016-10-02 23:08:52 -0300

    Add home controller

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  def login
+    @current_user = User.create(email: 'johndoe@email.com', password: '12345678')
+    sign_in @current_user
+  end
+
+  test '#index when user not logged' do
+    get :index
+    assert_response 200
+    assert_includes @response.body, 'Login'
+  end
+
+  test '#index when user is logged' do
+    login
+    get :index
+    assert_response 200
+    assert_includes @response.body, @current_user.email
+  end
+end
