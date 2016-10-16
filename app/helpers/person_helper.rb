7c0119f5bc2cff3443fa6cdbd46d49dc0059a90d
Vinicius Rodrigues <vini.rodrigues.90@gmail.com>
2016-10-16 18:37:41 -0200

    Add more options to person helper

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -1,17 +1,25 @@
 module PersonHelper
   def skin_options
-    ['', 'Negra', 'Branca']
+    ['', 'Albino', 'Amarela', 'Branca', 'Negra', 'Parda', 'Vermelha']
   end
 
   def hair_options
-    ['', 'Preto', 'Loiro']
+    ['', 'Aço', 'Branco', 'Castanho claro', 'Castanho escuro', 'Com mexas ou faixas', 'Louro', 'Preto', 'Ruivo']
   end
 
   def eyes_options
-    ['', 'Pretos', 'Acinzentados']
+    ['', 'Azuis', 'Castanhos claros', 'Castanhos escuros', 'Cinzentos', 'Desiguais na cor', 'Pretos', 'Verdes', 'Violetas']
   end
 
   def specificity_options
-    ['', 'Cicatriz', 'Tatuagem']
+    ['', 'Tatuagem', 'Cicatriz', 'Amputação', 'Deficiência']
+  end
+
+  def age_options
+    ['', 'até 18 anos', 'de 19 até 30 anos', 'de 31 até 45 anos', 'de 46 até 65 anos', 'acima de 65 anos']
+  end
+
+  def height_options
+    ['', 'Anão', 'até 1,60', 'de 1,60 até 1,70', 'de 1,70 até 1,80', 'acima 1,80']
   end
 end
